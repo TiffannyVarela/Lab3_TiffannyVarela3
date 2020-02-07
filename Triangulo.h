dc3ea551bd022c760d1b921ae4ecd9d0466212aa
TiffannyVarela <tiffanny_varela529@unitec.edu>
2020-02-07 13:32:29 -0600

    Segundo

--- Triangulo.h
+++ Triangulo.h
@@ -6,9 +6,26 @@ using namespace std;
 class Triangulo
 {
 	private:
+		int l1, l2, l3;
+		int high;
+		int Calculo(int,int,int);
 
 	public:
 		Triangulo();
+		Triangulo(int,int,int,int);
+
+		int getL1();
+		void setL1(int);
+
+		int getL2();
+                void setL2(int);
+
+		int getL3();
+                void setL3(int);
+
+		int getHigh();
+                void setHigh(int);
+
 		~Triangulo();
 	
 };
