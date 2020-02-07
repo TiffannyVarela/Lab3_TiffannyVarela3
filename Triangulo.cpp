371cd8e447cf3b896d20dcb2b1b6f4831499efe4
TiffannyVarela <tiffanny_varela529@unitec.edu>
2020-02-07 14:11:10 -0600

    Segundo

--- Triangulo.cpp
+++ Triangulo.cpp
@@ -1,2 +1,50 @@
 #include "Triangulo.h"
 #include <iostream>
+using namespace std;
+
+Triangulo::Triangulo(){
+}
+
+Triangulo::Triangulo(int l1, int l2, int l3, int high){
+	this -> l1=l1;
+	this -> l2=l2;
+	this -> l3=l3;
+	this -> high=high;
+
+}
+
+int Triangulo::getL1(){
+	return l1;
+}
+
+void Triangulo::setL1(int l1){
+	l1=l1;
+}
+
+int Triangulo::getL2(){
+	return l2;
+}
+
+void Triangulo::setL2(int l2){
+	l2=l2;
+}
+
+int Triangulo::getL3(){
+	return l3;
+}
+
+void Triangulo::setL3(int l3){
+	l3=l3;
+}
+
+int Triangulo::gethigh(){
+	return high;
+}
+
+void Triangulo::sethigh(int high){
+	high=high;
+}
+
+int Triangulo::Calculo(int l1, int l2, int l3){
+	
+}
--- main.cpp
+++ main.cpp
@@ -2,8 +2,34 @@
 using namespace std;
 
 int Ejercicio1(int);
+int menu();
 
 int main(){
+	int resp=-1;
+	int num;
+	do{
+		switch(menu()){
+			case 1:
+				cout<<"Ingrese un numero: ";
+				cin>>num;
+				cout<<Ejercicio1(num)<<endl;
+				break;
+
+			case 2:
+				break;
+
+			case 3:
+				break;
+
+			case 4:
+				break;
+
+			case 5:
+				break;
+		}
+		cout<<"Desea volver?: \n1.Si\n2.No"<<endl;
+		cin>>resp;
+	}while(resp!=2);
 	return 0;
 }
 
@@ -28,3 +54,36 @@ int menu(){//inicio metodo menu
  	}//end del while
         return 0;
 }//fin metodo menu
+
+int Ejercicio1(int num){
+	int op;
+	int result =0, result2=0, fin=0;;
+	bool resp=false;
+	for(int i=1; i<=num; i++){
+		op=(i*(i-1))/2;
+		if(op==num){
+			resp=true;
+			result=op;
+		//	cout<<"cont: "<<i<<endl;
+			break;
+		}
+		else{
+			resp=false;
+			if(op<num){
+				result2=op;
+			}
+		//	cout<<"contf: "<<i<<endl;
+		}
+	}
+
+	if(resp){
+		cout<<"El numero es Triangular: ";
+		fin=result;
+	}
+	else{
+		cout<<"El numero "<<num<<" no es triangular"<<endl;
+		cout<<"El triangular anterior es: ";
+		fin=result2;
+	}
+	return fin;
+}
